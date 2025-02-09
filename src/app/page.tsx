a162edd617160febdf063016c74e42e869076658
runerefund <blockchainsusanna@gmail.com>
2025-02-08 19:16:54 -0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ClaimForm from '../components/ClaimForm';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <ClaimForm />
+    </main>
+  );
+}
