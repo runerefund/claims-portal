e9413c5315332800d0f06aecd9d05c4483b307e0
runerefund <blockchainsusanna@gmail.com>
2025-02-08 19:12:29 -0800

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClaimForm from '../components/ClaimForm'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <ClaimForm />
-    </main>
-  )
-}
